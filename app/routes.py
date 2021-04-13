26c5d8ad30ad12cee2594a5153bc7f4603bdd8cb
Dan Holland <dholland@dev.caliraftdude.org>
2021-04-13 00:41:50 -0700

    Update templates to use base.html

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,16 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username':'Dan'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
 
+    return render_template('index.html', title='Home', user=user, posts=posts)
 

fa51153589718f6f76dded00f364116616b5612a
Dan Holland <dholland@dev.caliraftdude.org>
2021-04-13 02:57:31 -0700

    autocorrect screwing me with model vice Model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -8,4 +8,3 @@ class User(db.model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-        

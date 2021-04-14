98d994e8b39fa8b3de0bf9b08dda489527ecee39
Dan Holland <dholland@dev.caliraftdude.org>
2021-04-14 01:04:21 -0700

    Updates for User Logins (chapter 5)

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Flask Mega Tutorial
 - pip install flask-wtf
 - pip install flask-sqlalchemy
 - pip install flask-migrate
+- pip install flask-login
+- pip install email-validator
 
 ##Procedures:
 - flask run
--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from config import Config
+from flask_login import LoginManager
 
+
+# Application and Appliction configuration
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Database and Database migration management
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Login support and management
+login = LoginManager(app)
+login.login_view = 'login'
+
+# Avoid circular inclusion
 from app import routes, models
 

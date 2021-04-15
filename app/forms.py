b65964c52f98be1e62cb83d57fd2deffaa5db389
Dan Holland <dholland@dev.caliraftdude.org>
2021-04-15 00:26:25 -0700

    Added functionality for followers - Chapter 8

--- app/forms.py
+++ app/forms.py
@@ -41,5 +41,6 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError('Please use a different username')
 
-        
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
 
